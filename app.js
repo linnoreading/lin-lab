6a99121038e6924b283564f70c18269f6fb18ebb
fenghailin <fenghailin@outlook.com>
2020-01-15 15:12:10 +0800

    test

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 //regiter service worker
-alert("Test");
-if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('/lin-lab/sw.js',{scope:'/lin-lab/'}).then(function(reg){
-        if(reg.installing){
+console.log("Test")
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/lin-lab/sw.js', { scope: '/lin-lab/' }).then(function (reg) {
+        if (reg.installing) {
             console.log('Service Worker installing');
-        }else if(reg.waiting){
+        } else if (reg.waiting) {
             console.log('Service Worker installed');
-        }else if(reg.active){
+        } else if (reg.active) {
             console.log('Service Worker active');
         }
-    }).catch(function(error){
+    }).catch(function (error) {
         //registration failed
-        console.log('Regisitration failed with '+error);
+        console.log('Regisitration failed with ' + error);
     })
 }
 
--- sw.js
+++ sw.js
@@ -1,35 +1,34 @@
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('v1').then(function(cache) {
-        return cache.addAll([
-          '/lin-lab/',
-          '/lin-lab/Lib/scripts/Core/jquery-3.1.0.min.js'
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(caches.match(event.request).then(function(response) {
-      // caches.match() always resolves
-      // but in case of success response will have value
-      if (response !== undefined) {
-        return response;
-      } else {
-        return fetch(event.request).then(function (response) {
-          // response may be used only once
-          // we need to save clone to put one copy in cache
-          // and serve second one
-          let responseClone = response.clone();
-          
-          caches.open('v1').then(function (cache) {
-            cache.put(event.request, responseClone);
-          });
-          return response;
-        }).catch(function () {
-          return caches.match('/lin-lab/xu/images/loading.gif');
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open('v1').then(function (cache) {
+      return cache.addAll([
+        '/lin-lab/',
+        '/lin-lab/Lib/scripts/Core/jquery-3.1.0.min.js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(caches.match(event.request).then(function (response) {
+    // caches.match() always resolves
+    // but in case of success response will have value
+    if (response !== undefined) {
+      return response;
+    } else {
+      return fetch(event.request).then(function (response) {
+        // response may be used only once
+        // we need to save clone to put one copy in cache
+        // and serve second one
+        let responseClone = response.clone();
+
+        caches.open('v1').then(function (cache) {
+          cache.put(event.request, responseClone);
         });
-      }
-    }));
-  });
-  
+        return response;
+      }).catch(function () {
+        return caches.match('/lin-lab/xu/images/loading.gif');
+      });
+    }
+  }));
+});
