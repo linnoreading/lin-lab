4e1aa1abbdd2de80ac2334c07993db135d5b3250
fenghailin <fenghailin@outlook.com>
2020-01-15 11:39:59 +0800

    test service worker

---
+++ app.js
@@ -0,0 +1,17 @@
+//regiter service worker
+
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('/lin-lab/sw.js',{scope:'/lin-lab/'}).then(function(reg){
+        if(reg.installing){
+            console.log('Service Worker installing');
+        }else if(reg.waiting){
+            console.log('Service Worker installed');
+        }else if(reg.active){
+            console.log('Service Worker active');
+        }
+    }).catch(function(error){
+        //registration failed
+        console.log('Regisitration failed with '+error);
+    })
+}
+
