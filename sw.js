1d81b836a7313681b87392ba222dcfabdaf4b1fe
fenghailin <fenghailin@outlook.com>
2020-01-15 17:37:52 +0800

    test

--- sw.js
+++ sw.js
@@ -3,7 +3,8 @@ self.addEventListener('install', function (event) {
     caches.open('v1').then(function (cache) {
       return cache.addAll([
         '/lin-lab/',
-        '/lin-lab/Lib/scripts/Core/jquery-3.1.0.min.js'
+        '/lin-lab/Lib/scripts/Core/jquery-3.1.0.min.js',
+        '/lin-lab/xu2/'
       ]);
     })
   );
